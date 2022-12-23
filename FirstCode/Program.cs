8ba2b3c51ae8b6f87ce3c53ec15e445f2eab4e47
Arike115 <ajibolaoluwafunke669@gmail.com>
2022-12-23 15:41:25 +0100

    Add project files.

---
+++ FirstCode/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FirstCode;
+
+Console.WriteLine("Hello, World!");
+int v = 45;
+Console.WriteLine(v);
+
+//object
+//instatiation 
+Student std = new Student();
+std.Name = "Venkat";
+std.Age = 35;
+std.Gender = "Male";
+std.GetList();
+
+
+string word = "hello";
+//array
+string[] words = { "hello", "church", "chalk", "yoga" };
---
+++ FirstCode/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCode
+{
+    //access modifiers
+    // private
+    //public
+    //internal
+    //protected
+
+    //Static and Nonstatic class
+
+   public class Student
+    {
+        //property
+        public int Id { get; set; }
+
+        //field
+        public string Name;
+        public int Age;
+        public string Gender;
+        
+
+        //Constructor
+        //public Student(string name, int age, string gender) 
+        //{
+        //    Name = name;
+        //    Age = age;
+        //    Gender = gender;
+        //}
+
+
+
+        public void GetList()
+        {
+            
+            Console.WriteLine("{0}-----{1}-----{2}", Name, Age, Gender);
+        }
+    }
+}
